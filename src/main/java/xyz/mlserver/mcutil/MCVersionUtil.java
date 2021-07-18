44d4ac814378913f63795fa418066cfaa033de60
Monster2408 <meoto2408@gmail.com>
2021-07-18 20:50:15 +0900

    バージョン関連追加

--- src/main/java/xyz/mlserver/mcutil/MCVersionUtil.java
+++ src/main/java/xyz/mlserver/mcutil/MCVersionUtil.java
@@ -1,11 +1,32 @@
 package xyz.mlserver.mcutil;
 
+import com.viaversion.viaversion.api.Via;
+import org.bukkit.entity.Player;
+import util.ICollectionList;
 import xyz.acrylicstyle.mcutil.lang.MCVersion;
+
+import java.util.Objects;
 
 public class MCVersionUtil {
 
+    /**
+     *
+     * @param protocolVersion
+     * @return
+     */
     public static String getByProtocolVersion(int protocolVersion) {
         return MCVersion.getByProtocolVersion(protocolVersion).get(0).getName();
+    }
+
+    public static String getByProtocolVersion(Player player) {
+        return getReleaseVersionIfPossible(Via.getAPI().getPlayerVersion(player.getUniqueId())).getName();
+    }
+
+    public static MCVersion getReleaseVersionIfPossible(int protocolVersion) {
+        ICollectionList<MCVersion> list = ICollectionList.asList(MCVersion.getByProtocolVersion(protocolVersion));
+        return list.filter(v -> !v.isSnapshot()).size() == 0 // if non-snapshot version wasn't found
+                ? Objects.requireNonNull(list.first()) // return the last version anyway
+                : Objects.requireNonNull(list.filter(v -> !v.isSnapshot()).first()); // return non-snapshot version instead
     }
 
 }
