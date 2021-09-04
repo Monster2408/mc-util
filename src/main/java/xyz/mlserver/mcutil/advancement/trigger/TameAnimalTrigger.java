47196efeb88ad37162c82d0679bed5f2154072ae
Monster2408 <meoto2408@gmail.com>
2021-09-04 22:26:58 +0900

    Advancementの追加

---
+++ src/main/java/xyz/mlserver/mcutil/advancement/trigger/TameAnimalTrigger.java
@@ -0,0 +1,45 @@
+package xyz.mlserver.mcutil.advancement.trigger;
+
+import com.google.gson.JsonObject;
+import org.jetbrains.annotations.Nullable;
+import xyz.mlserver.mcutil.advancement.shared.EntityObject;
+import xyz.mlserver.mcutil.advancement.util.JsonBuilder;
+
+/**
+ * Fires whenever the player tames an animal.
+ */
+@SuppressWarnings({"unused", "SameParameterValue"})
+public class TameAnimalTrigger extends Trigger {
+	@Nullable EntityObject entity = null;
+	
+	public TameAnimalTrigger() {
+		super(Type.TAME_ANIMAL);
+	}
+	
+	
+	
+	/**
+	 * @return information about the tamed entity or null, if none was specified
+	 */
+	public @Nullable EntityObject getEntity() {
+		return entity;
+	}
+	
+	/**
+	 * @param entity information about the tamed entity or null, if it should be cleared
+	 * @return the current trigger for chaining
+	 */
+	public TameAnimalTrigger setEntity(@Nullable EntityObject entity) {
+		this.entity = entity;
+		return this;
+	}
+	
+	
+	
+	@Override
+	protected JsonObject getConditions() {
+		return entity == null ? null : new JsonBuilder()
+				.add("entity", entity)
+				.build();
+	}
+}
