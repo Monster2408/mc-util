0e618b4db5b98358f11c032d6c55f10dfa253d03
Monster2408 <meoto2408@gmail.com>
2021-09-22 22:03:12 +0900

    Release Development Version 1.2.14-SNAPSHOT-7

--- src/main/java/xyz/mlserver/mc/util/CmdUtil.java
+++ src/main/java/xyz/mlserver/mc/util/CmdUtil.java
@@ -82,13 +82,18 @@ public class CmdUtil {
      * @param advanced
      */
     public void send(Player player, int page, boolean advanced) {
-        if (advanced) for (TextComponent textComponent : getAdvanceHelpMsg(page)) player.spigot().sendMessage(textComponent);
-        else for (String text : getHelpMsg(page)) player.sendMessage(text);
+        if (player.isOp()) {
+            if (advanced) for (TextComponent textComponent : getAdminAdvanceHelpMsg(page)) player.spigot().sendMessage(textComponent);
+            else for (String text : getAdminHelpMsg(page)) player.sendMessage(text);
+        } else {
+            if (advanced) for (TextComponent textComponent : getAdvanceHelpMsg(page)) player.spigot().sendMessage(textComponent);
+            else for (String text : getHelpMsg(page)) player.sendMessage(text);
+        }
     }
 
     public void send(CommandSender sender, int page) {
         if (sender instanceof Player) send(((Player)sender), page, false);
-        else for (String text : getHelpMsg(page)) sender.spigot().sendMessage(new TextComponent(text));
+        else for (String text : getAdminHelpMsg(page)) sender.sendMessage(text);
     }
 
     public void send(Player player, int page) {
@@ -143,11 +148,16 @@ public class CmdUtil {
         String text;
         HashMap<String, String> hash = this.getCommandUtilHash();
         HashMap<String, String> adminHash = this.getAdminCommandUtilHash();
-        for (String cmd : adminHash.keySet()) hash.put(cmd, adminHash.get(cmd));
         for (String cmd : hash.keySet()) {
             text = format;
             text = text.replace("%CMD%", cmd);
             text = text.replace("%DESCRIPTION%", hash.get(cmd));
+            list.add(text);
+        }
+        for (String cmd : adminHash.keySet()) {
+            text = format;
+            text = text.replace("%CMD%", cmd);
+            text = text.replace("%DESCRIPTION%", adminHash.get(cmd));
             list.add(text);
         }
         int max;
@@ -184,11 +194,60 @@ public class CmdUtil {
         String text;
         HashMap<String, String> hash = this.getCommandUtilHash();
         HashMap<String, String> adminHash = this.getAdminCommandUtilHash();
-        for (String cmd : adminHash.keySet()) hash.put(cmd, adminHash.get(cmd));
         for (String cmd : hash.keySet()) {
             text = format;
             text = text.replace("%CMD%", cmd);
             text = text.replace("%DESCRIPTION%", hash.get(cmd));
+            component = new TextComponent(text);
+            component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, cmd));
+            list.add(component);
+        }
+        int max;
+        int COMMAND_LINES = 7;
+        List<TextComponent> _list = new ArrayList<>();
+        if ((list.size() % COMMAND_LINES) > 0) {
+            max = (list.size() / COMMAND_LINES) + 1;
+        } else {
+            max = (list.size() / COMMAND_LINES);
+        }
+        int ListI;
+        if (max < page) {
+            ListI = 0;
+            page = 1;
+        } else {
+            ListI = page - 1;
+        }
+        ListI = ListI * COMMAND_LINES;
+        text = header;
+        text = text.replace("%NOW-PAGE%", String.valueOf(page));
+        text = text.replace("%MAX-PAGE%", String.valueOf(max));
+        _list.add(new TextComponent(text));
+        for (int n = 0; n < COMMAND_LINES; n++) {
+            if (ListI >= list.size()) return _list;
+            _list.add(list.get(ListI));
+            ListI++;
+        }
+        return _list;
+    }
+
+    public List<TextComponent> getAdminAdvanceHelpMsg(int page) {
+        List<TextComponent> list = new ArrayList<>();
+        TextComponent component;
+        String text;
+        HashMap<String, String> hash = this.getCommandUtilHash();
+        HashMap<String, String> adminHash = this.getAdminCommandUtilHash();
+        for (String cmd : hash.keySet()) {
+            text = format;
+            text = text.replace("%CMD%", cmd);
+            text = text.replace("%DESCRIPTION%", hash.get(cmd));
+            component = new TextComponent(text);
+            component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, cmd));
+            list.add(component);
+        }
+        for (String cmd : adminHash.keySet()) {
+            text = format;
+            text = text.replace("%CMD%", cmd);
+            text = text.replace("%DESCRIPTION%", adminHash.get(cmd));
             component = new TextComponent(text);
             component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, cmd));
             list.add(component);
