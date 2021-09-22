0454dde2d9f685c760f73f3b1f836ea30ee20974
Monster2408 <meoto2408@gmail.com>
2021-09-22 21:35:29 +0900

    Release Development Version 1.2.14-SNAPSHOT-5

--- src/main/java/xyz/mlserver/mc/util/CmdUtil.java
+++ src/main/java/xyz/mlserver/mc/util/CmdUtil.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class CmdUtil {
 
     private final HashMap<String, String> commandUtilHash;
+    private final HashMap<String, String> adminCommandUtilHash;
     private String format;
     private String header;
 
@@ -21,24 +22,32 @@ public class CmdUtil {
 
     public CmdUtil(String format, String header) {
         this.commandUtilHash = new HashMap<>();
+        this.adminCommandUtilHash = new HashMap<>();
         this.format = format;
         this.header = header;
     }
 
     public CmdUtil(String format) {
         this.commandUtilHash = new HashMap<>();
+        this.adminCommandUtilHash = new HashMap<>();
         this.format = format;
         this.header = temp_heder;
     }
 
     public CmdUtil() {
         this.commandUtilHash = new HashMap<>();
+        this.adminCommandUtilHash = new HashMap<>();
         this.format = temp_format;
         this.header = temp_heder;
     }
 
     public CmdUtil add(String cmd, String description) {
         this.commandUtilHash.put(cmd, description);
+        return this;
+    }
+
+    public CmdUtil addOP(String cmd, String description) {
+        this.adminCommandUtilHash.put(cmd, description);
         return this;
     }
 
@@ -60,6 +69,10 @@ public class CmdUtil {
 
     public HashMap<String, String> getCommandUtilHash() {
         return commandUtilHash;
+    }
+
+    public HashMap<String, String> getAdminCommandUtilHash() {
+        return adminCommandUtilHash;
     }
 
     /**
@@ -125,11 +138,53 @@ public class CmdUtil {
         return _list;
     }
 
+    public List<String> getAdminHelpMsg(int page) {
+        List<String> list = new ArrayList<>();
+        String text;
+        HashMap<String, String> hash = this.getCommandUtilHash();
+        HashMap<String, String> adminHash = this.getAdminCommandUtilHash();
+        for (String cmd : adminHash.keySet()) hash.put(cmd, adminHash.get(cmd));
+        for (String cmd : hash.keySet()) {
+            text = format;
+            text = text.replace("%CMD%", cmd);
+            text = text.replace("%DESCRIPTION%", hash.get(cmd));
+            list.add(text);
+        }
+        int max;
+        int COMMAND_LINES = 7;
+        List<String> _list = new ArrayList<>();
+        if ((list.size() % COMMAND_LINES) > 0) {
+            max = (list.size() / COMMAND_LINES) + 1;
+        } else {
+            max = (list.size() / COMMAND_LINES);
+        }
+        int ListI;
+        if (max < page) {
+            ListI = 0;
+            page = 1;
+        } else {
+            ListI = page - 1;
+        }
+        ListI = ListI * COMMAND_LINES;
+        text = header;
+        text = text.replace("%NOW-PAGE%", String.valueOf(page));
+        text = text.replace("%MAX-PAGE%", String.valueOf(max));
+        _list.add(text);
+        for (int n = 0; n < COMMAND_LINES; n++) {
+            if (ListI >= list.size()) return _list;
+            _list.add(list.get(ListI));
+            ListI++;
+        }
+        return _list;
+    }
+
     public List<TextComponent> getAdvanceHelpMsg(int page) {
         List<TextComponent> list = new ArrayList<>();
         TextComponent component;
         String text;
         HashMap<String, String> hash = this.getCommandUtilHash();
+        HashMap<String, String> adminHash = this.getAdminCommandUtilHash();
+        for (String cmd : adminHash.keySet()) hash.put(cmd, adminHash.get(cmd));
         for (String cmd : hash.keySet()) {
             text = format;
             text = text.replace("%CMD%", cmd);
