eef4a46fd13b9c184195ae2cac3e761c760b83af
Monster2408 <meoto2408@gmail.com>
2021-09-27 06:54:24 +0900

    Languageにidを追加

--- src/main/java/xyz/mlserver/lang/Language.java
+++ src/main/java/xyz/mlserver/lang/Language.java
@@ -1,15 +1,19 @@
 package xyz.mlserver.lang;
 
 public enum Language {
-    ENGLISH("ENGLISH"),
-    JAPANESE("日本語") {
+    ENGLISH("ENGLISH", "english"),
+    JAPANESE("日本語", "japanese") {
         @Override
         public String toString() {
             return name().toUpperCase();
         }
     };
-    private String name;
-    Language(String name) { this.name = name; }
+    private String name, id;
+    Language(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 
     public String getName() { return name; }
+    public String getId() { return id; }
 }
