09fb0f378ce89183c95de1591fe88d74d0327f59
Monster2408 <meoto2408@gmail.com>
2021-12-16 22:34:11 +0900

    ADD 1.18.1 rc2/3/release

--- src/main/java/xyz/mlserver/mc/util/version/MCVersion.java
+++ src/main/java/xyz/mlserver/mc/util/version/MCVersion.java
@@ -12,24 +12,13 @@ public enum MCVersion {
         https://minecraft.fandom.com/wiki/Protocol_version#Java_Edition
     */
 
-//    Java Edition 1.18.1 Release Candidate 1	0x4000003E	2862
-//    Java Edition 1.18.1 Pre-release 1	0x4000003D	2861
-//    Java Edition 1.18	757	2860
-//    Java Edition 1.18 Release Candidate 4	0x4000003C	2859
-//    Java Edition 1.18 Release Candidate 3	0x4000003B	2858
-//    Java Edition 1.18 Release Candidate 2	0x4000003A	2857
-//    Java Edition 1.18 Release Candidate 1	0x40000039	2856
-//    Java Edition 1.18 Pre-release 8	0x40000038	2855
-//    Java Edition 1.18 Pre-release 7	0x40000037	2854
-//    Java Edition 1.18 Pre-release 6	0x40000036	2853
-//    Java Edition 1.18 Pre-release 5	0x40000035	2851
-//    Java Edition 1.18 Pre-release 4	0x40000034	2850
-//    Java Edition 1.18 Pre-release 3	0x40000033	2849
-//    Java Edition 1.18 Pre-release 2	0x40000032	2848
-//    Java Edition 1.18 Pre-release 1	0x40000031	2847
-//    Java Edition 21w44a	0x40000030	2845
+//    Java Edition 1.18.1	757	2865
+//    Java Edition 1.18.1 Release Candidate 3	0x40000040	2864
+//    Java Edition 1.18.1 Release Candidate 2	0x4000003F	2863
 
-    v1_18_1     (1, -1, "1.18.1", VersionType.DUMMY),
+    v1_18_1     (1, 757, "1.18.1", VersionType.RELEASE),
+    v1_18_1_RC3 (0x40000040,2864,v1_18_1,"1.18.1-rc3",  VersionType.RELEASE_CANDIDATE),
+    v1_18_1_RC2 (0x4000003F,2863,v1_18_1,"1.18.1-rc2",  VersionType.RELEASE_CANDIDATE),
     v1_18_1_RC1 (0x4000003E,2862,v1_18_1,"1.18.1-rc1",  VersionType.RELEASE_CANDIDATE),
     v1_18_1_PRE1(0x4000003D,2861,v1_18_1,"1.18.1-pre1", VersionType.PRERELEASE),
 
