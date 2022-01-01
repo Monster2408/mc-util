c44ebb2290057f34ccb0b26c1dfc049e77287037
Monster2408 <meoto2408@gmail.com>
2022-01-01 13:53:17 +0900

    add get minecraft player name from player uuid

--- src/main/java/xyz/mlserver/mc/util/Mojang.java
+++ src/main/java/xyz/mlserver/mc/util/Mojang.java
@@ -2,56 +2,62 @@ package xyz.mlserver.mc.util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
+
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.Objects;
 import java.util.UUID;
 
 public class Mojang {
-    public static UUID getUUID(String playerName) {
 
+    public static UUID getUUID(String playerName) {
         // URLを叩くとラグが発生するため
         for (Player p : Bukkit.getOnlinePlayers()) if (p.getName().equalsIgnoreCase(playerName)) return p.getUniqueId();
-
         String output = callURL("https://api.mojang.com/users/profiles/minecraft/" + playerName);
-
         StringBuilder result = new StringBuilder();
-
         readData(output, result);
-
         String u = result.toString();
-
         System.out.println(u);
-
         StringBuilder uuid = new StringBuilder();
-
         for(int i = 0; i <= 31; i++) {
             uuid.append(u.charAt(i));
-            if(i == 7 || i == 11 || i == 15 || i == 19) {
-                uuid.append("-");
-            }
+            if(i == 7 || i == 11 || i == 15 || i == 19) uuid.append("-");
         }
-
         return UUID.fromString(uuid.toString());
+    }
+
+    public String getName(String uuid) {
+        String url = "https://api.mojang.com/user/profiles/"+uuid.replace("-", "")+"/names";
+        try {
+            @SuppressWarnings("deprecation")
+            String nameJson = IOUtils.toString(new URL(url));
+            JSONArray nameValue = (JSONArray) JSONValue.parseWithException(nameJson);
+            String playerSlot = nameValue.get(nameValue.size()-1).toString();
+            JSONObject nameObject = (JSONObject) JSONValue.parseWithException(playerSlot);
+            return nameObject.get("name").toString();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return "error";
     }
 
     private static void readData(String toRead, StringBuilder result) {
         int i = 7;
-
         while(i < 200) {
-            if(!String.valueOf(toRead.charAt(i)).equalsIgnoreCase("\"")) {
-
-                result.append(toRead.charAt(i));
-
-            } else {
-                break;
-            }
-
+            if(!String.valueOf(toRead.charAt(i)).equalsIgnoreCase("\"")) result.append(toRead.charAt(i));
+            else break;
             i++;
         }
     }
@@ -63,29 +69,18 @@ public class Mojang {
         try {
             java.net.URL url = new URL(URL);
             urlConn = url.openConnection();
-
             if (urlConn != null) urlConn.setReadTimeout(60 * 1000);
-
             if (urlConn != null && urlConn.getInputStream() != null) {
                 in = new InputStreamReader(urlConn.getInputStream(), Charset.defaultCharset());
                 BufferedReader bufferedReader = new BufferedReader(in);
-
-                if (bufferedReader != null) {
-                    int cp;
-
-                    while ((cp = bufferedReader.read()) != -1) {
-                        sb.append((char) cp);
-                    }
-
-                    bufferedReader.close();
-                }
+                int cp;
+                while ((cp = bufferedReader.read()) != -1) sb.append((char) cp);
+                bufferedReader.close();
             }
-
             Objects.requireNonNull(in).close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return sb.toString();
     }
 }
