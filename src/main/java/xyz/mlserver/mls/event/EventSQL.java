35caa724ce2d3b2e5247248ed101100dedfb0be2
monster2408 <meoto2408@gmail.com>
2022-08-15 02:03:48 +0900

    add comment and system

--- src/main/java/xyz/mlserver/mls/event/EventSQL.java
+++ src/main/java/xyz/mlserver/mls/event/EventSQL.java
@@ -16,158 +16,75 @@ public class EventSQL {
 
     private final DataBase dataBase;
     private final String table_name;
-    private final HashMap<String, Integer> winData;
-    private final HashMap<String, Integer> gameData;
 
+    /**
+     * イベントクラス({@link MLSEvent})に登録されていない場合などに利用
+     * @param table_name イベントのテーブル名を指定
+     * @param dataBase データベース{@link DataBase}を指定
+     * @deprecated 全てが綺麗に動作するかがわからないため非推奨
+     */
     public EventSQL(String table_name, DataBase dataBase) {
         this.table_name = table_name;
         this.dataBase = dataBase;
-        this.winData = new HashMap<>();
-        this.gameData = new HashMap<>();
     }
 
+    /**
+     * イベントクラス({@link MLSEvent})から利用
+     * @param event イベントを指定
+     * @param dataBase データベース{@link DataBase}を指定
+     */
     public EventSQL(MLSEvent event, DataBase dataBase) { this(event.getDatabase(), dataBase); }
 
+    /**
+     * 企画の勝利数をUUID({@link String})より取得
+     * @param uuid UUID({@link String})を指定
+     * @return 企画の勝利数を取得
+     */
     public int getWin(String uuid) {
         if (table_name == null) return -1;
 
         createTable();
-        if (winData.get(uuid) != null) return winData.get(uuid);
+        int num;
         String sql = "select * from " + table_name + " where uuid=?";
         try(Connection con = dataBase.getDataSource().getConnection();
             PreparedStatement prestat = con.prepareStatement(sql)) {
             prestat.setString(1, uuid);
             ResultSet result = prestat.executeQuery();
             if (result.next()) {
-                int num = result.getInt(3);
-                winData.put(uuid, num);
+                num = result.getInt(3);
             } else {
-                winData.putIfAbsent(uuid, 0);
+                num = 0;
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            winData.putIfAbsent(uuid, 0);
+            num = 0;
         }
-        return winData.get(uuid);
+        return num;
     }
 
+    /**
+     * 企画の勝利数をUUID({@link UUID})より取得
+     * @param uuid UUID({@link UUID})を指定
+     * @return 企画の勝利数を取得
+     */
     public int getWin(UUID uuid) {
         return getWin(uuid.toString());
     }
+    /**
+     * 企画の勝利数をplayer({@link Player})より取得
+     * @param player player({@link Player})を指定
+     * @return 企画の勝利数を取得
+     */
     public int getWin(Player player) {
         return getWin(player.getUniqueId());
     }
 
-    public void addWin(String uuid, int i) {
-        if (table_name == null) return ;
-
-        getGame(uuid);
-        getWin(uuid);
-        winData.put(uuid, winData.get(uuid) + i);
-
-        String sql = "insert into " + table_name + " (uuid, game, win) "
-                + "VALUES (?, ?, ?) "
-                +"ON DUPLICATE KEY UPDATE "
-                +"uuid=?, "
-                +"game=?, "
-                +"win=?;";
-        try(Connection con = dataBase.getDataSource().getConnection();
-            PreparedStatement prestat = con.prepareStatement(sql)) {
-            prestat.setString(1, uuid);
-            prestat.setInt(2, gameData.get(uuid));
-            prestat.setInt(3, winData.get(uuid));
-            prestat.setString(4, uuid);
-            prestat.setInt(5, gameData.get(uuid));
-            prestat.setInt(6, winData.get(uuid));
-            prestat.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void addWin(String uuid) { addWin(uuid, 1); }
-    public void addWin(UUID uuid) { addWin(uuid.toString(), 1); }
-
-    public int getGame(String uuid) {
-        if (table_name == null) return -1;
-
-        createTable();
-        if (gameData.get(uuid) != null) return gameData.get(uuid);
-        String sql = "select * from " + table_name + " where uuid=?";
-        try(Connection con = dataBase.getDataSource().getConnection();
-            PreparedStatement prestat = con.prepareStatement(sql)) {
-            prestat.setString(1, uuid);
-            ResultSet result = prestat.executeQuery();
-            if (result.next()) {
-                int num = result.getInt(2);
-                gameData.put(uuid, num);
-            } else {
-                gameData.putIfAbsent(uuid, 0);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            gameData.putIfAbsent(uuid, 0);
-        }
-        return gameData.get(uuid);
-    }
-
-    public int getGame(UUID uuid) {
-        return getGame(uuid.toString());
-    }
-
-    public void addGame(String uuid, int i) {
-        if (table_name == null) return ;
-
-        getGame(uuid);
-        getWin(uuid);
-        gameData.put(uuid, gameData.get(uuid) + i);
-
-        String sql = "insert into " + table_name + " (uuid, game, win) "
-                + "VALUES (?, ?, ?) "
-                +"ON DUPLICATE KEY UPDATE "
-                +"uuid=?, "
-                +"game=?, "
-                +"win=?;";
-        try(Connection con = dataBase.getDataSource().getConnection();
-            PreparedStatement prestat = con.prepareStatement(sql)) {
-            prestat.setString(1, uuid);
-            prestat.setInt(2, gameData.get(uuid));
-            prestat.setInt(3, winData.get(uuid));
-            prestat.setString(4, uuid);
-            prestat.setInt(5, gameData.get(uuid));
-            prestat.setInt(6, winData.get(uuid));
-            prestat.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void addGame(String uuid) { addGame(uuid, 1); }
-    public void addGame(UUID uuid) { addGame(uuid.toString(), 1); }
-
-    public HashMap<String, Integer> getWinData() {
-        return winData;
-    }
-
-    public HashMap<String, Integer> getGameData() {
-        return gameData;
-    }
-
-    private void createTable() {
-        String sql = "create table if not exists " + table_name + " (" +
-                "uuid text NOT NULL PRIMARY KEY," +
-                "game int(1000) default '0' not null" +
-                "win int(1000) default '0' not null" +
-                ");"
-                ;
-        try(Connection con = dataBase.getDataSource().getConnection();
-            PreparedStatement prestat = con.prepareStatement(sql)) {
-            prestat.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
+    /**
+     * 指定した企画の勝利数をUUID({@link String})より取得
+     * @param event 企画({@link MLSEvent})を指定
+     * @param uuid UUID({@link String})を指定
+     * @return 企画の勝利数を取得
+     */
     public int getWin(MLSEvent event, String uuid) {
         createTable(event);
 
@@ -179,7 +96,6 @@ public class EventSQL {
             ResultSet result = prestat.executeQuery();
             if (result.next()) {
                 num = result.getInt(3);
-                winData.put(uuid, num);
             } else {
                 num = 0;
             }
@@ -190,13 +106,139 @@ public class EventSQL {
         return num;
     }
 
+    /**
+     * 指定した企画の勝利数をUUID({@link UUID})より取得
+     * @param event 企画({@link MLSEvent})を指定
+     * @param uuid UUID({@link UUID})を指定
+     * @return 企画の勝利数を取得
+     */
     public int getWin(MLSEvent event, UUID uuid) {
         return getWin(event, uuid.toString());
     }
+
+    /**
+     * 指定した企画の勝利数をplayer({@link Player})より取得
+     * @param event 企画({@link MLSEvent})を指定
+     * @param player プレイヤー({@link Player})を指定
+     * @return 企画の勝利数を取得
+     */
     public int getWin(MLSEvent event, Player player) {
         return getWin(event, player.getUniqueId());
     }
 
+    /**
+     * UUID({@link String})を元に企画の勝利数を指定数増やす
+     * @param uuid　UUID({@link String})を指定
+     * @param i 増やす勝利数を指定
+     */
+    public void addWin(String uuid, int i) {
+        if (table_name == null) return ;
+
+        int game = getGame(uuid);
+        int win = getWin(uuid) + i;
+
+        String sql = "insert into " + table_name + " (uuid, game, win) "
+                + "VALUES (?, ?, ?) "
+                +"ON DUPLICATE KEY UPDATE "
+                +"uuid=?, "
+                +"game=?, "
+                +"win=?;";
+        try(Connection con = dataBase.getDataSource().getConnection();
+            PreparedStatement prestat = con.prepareStatement(sql)) {
+            prestat.setString(1, uuid);
+            prestat.setInt(2, game);
+            prestat.setInt(3, win);
+            prestat.setString(4, uuid);
+            prestat.setInt(5, game);
+            prestat.setInt(6, win);
+            prestat.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * UUID({@link UUID})を元に企画の勝利数を指定数増やす
+     * @param uuid UUID({@link UUID})を指定
+     * @param i 増やす勝利数を指定
+     */
+    public void addWin(UUID uuid, int i) { addWin(uuid.toString(), i); }
+
+    /**
+     * プレイヤー({@link Player})を元に企画の勝利数を指定数増やす
+     * @param player プレイヤー({@link Player})を指定
+     * @param i 増やす勝利数を指定
+     */
+    public void addWin(Player player, int i) { addWin(player.getUniqueId(), i); }
+
+    /**
+     * UUID({@link String})を元に企画の勝利数を1増やす
+     * @param uuid UUID({@link String})を指定
+     */
+    public void addWin(String uuid) { addWin(uuid, 1); }
+
+    /**
+     * UUID({@link UUID})を元に企画の勝利数を1増やす
+     * @param uuid UUID({@link UUID})を指定
+     */
+    public void addWin(UUID uuid) { addWin(uuid.toString()); }
+
+    /**
+     * プレイヤー({@link Player})を元に企画の勝利数を1増やす
+     * @param player プレイヤー({@link Player})を指定
+     */
+    public void addWin(Player player) { addWin(player.getUniqueId()); }
+
+    /**
+     * 企画の参加数をUUID({@link String})より取得
+     * @param uuid UUID({@link String})を指定
+     * @return 企画の参加数を取得
+     */
+    public int getGame(String uuid) {
+        if (table_name == null) return -1;
+        createTable();
+
+        int num;
+        String sql = "select * from " + table_name + " where uuid=?";
+        try(Connection con = dataBase.getDataSource().getConnection();
+            PreparedStatement prestat = con.prepareStatement(sql)) {
+            prestat.setString(1, uuid);
+            ResultSet result = prestat.executeQuery();
+            if (result.next()) {
+                num = result.getInt(2);
+            } else {
+                num = 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            num = 0;
+        }
+        return num;
+    }
+
+    /**
+     * 企画の参加数をUUID({@link UUID})より取得
+     * @param uuid UUID({@link UUID})を指定
+     * @return 企画の参加数を取得
+     */
+    public int getGame(UUID uuid) {
+        return getGame(uuid.toString());
+    }
+    /**
+     * 企画の参加数をplayer({@link Player})より取得
+     * @param player player({@link Player})を指定
+     * @return 企画の参加数を取得
+     */
+    public int getGame(Player player) {
+        return getGame(player.getUniqueId());
+    }
+
+    /**
+     * 指定した企画の参加数をUUID({@link String})より取得
+     * @param event 企画({@link MLSEvent})を指定
+     * @param uuid UUID({@link String})を指定
+     * @return 企画の参加数を取得
+     */
     public int getGame(MLSEvent event, String uuid) {
         createTable(event);
 
@@ -218,13 +260,93 @@ public class EventSQL {
         return num;
     }
 
+    /**
+     * 指定した企画の参加数をUUID({@link UUID})より取得
+     * @param event 企画({@link MLSEvent})を指定
+     * @param uuid UUID({@link UUID})を指定
+     * @return 企画の参加数を取得
+     */
     public int getGame(MLSEvent event, UUID uuid) {
         return getGame(event, uuid.toString());
     }
+    /**
+     * 指定した企画の参加数をplayer({@link Player})より取得
+     * @param event 企画({@link MLSEvent})を指定
+     * @param player プレイヤー({@link Player})を指定
+     * @return 企画の参加数を取得
+     */
     public int getGame(MLSEvent event, Player player) {
         return getGame(event, player.getUniqueId());
     }
 
+    /**
+     * UUID({@link String})を元に企画の参加数を指定数増やす
+     * @param uuid UUID({@link String})を指定
+     * @param i 増やす勝利数を指定
+     */
+    public void addGame(String uuid, int i) {
+        if (table_name == null) return ;
+
+        int game = getGame(uuid) + i;
+        int win = getWin(uuid);
+
+        String sql = "insert into " + table_name + " (uuid, game, win) "
+                + "VALUES (?, ?, ?) "
+                +"ON DUPLICATE KEY UPDATE "
+                +"uuid=?, "
+                +"game=?, "
+                +"win=?;";
+        try(Connection con = dataBase.getDataSource().getConnection();
+            PreparedStatement prestat = con.prepareStatement(sql)) {
+            prestat.setString(1, uuid);
+            prestat.setInt(2, game);
+            prestat.setInt(3, win);
+            prestat.setString(4, uuid);
+            prestat.setInt(5, game);
+            prestat.setInt(6, win);
+            prestat.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * UUID({@link UUID})を元に企画の参加数を指定数増やす
+     * @param uuid UUID({@link UUID})を指定
+     * @param i 増やす勝利数を指定
+     */
+    public void addGame(UUID uuid, int i) { addGame(uuid.toString(), i); }
+
+    /**
+     * プレイヤー({@link Player})を元に企画の参加数を指定数増やす
+     * @param player プレイヤー({@link Player})を指定
+     * @param i 増やす勝利数を指定
+     */
+    public void addGame(Player player, int i) { addGame(player.getUniqueId(), i); }
+
+    /**
+     * UUID({@link String})を元に企画の参加数を1増やす
+     * @param uuid UUID({@link String})を指定
+     */
+    public void addGame(String uuid) { addGame(uuid, 1); }
+
+    /**
+     * UUID({@link UUID})を元に企画の参加数を1増やす
+     * @param uuid UUID({@link UUID})を指定
+     */
+    public void addGame(UUID uuid) { addGame(uuid.toString()); }
+
+    /**
+     * プレイヤー({@link Player})を元に企画の参加数を1増やす
+     * @param player プレイヤー({@link Player})を指定
+     */
+    public void addGame(Player player) { addGame(player.getUniqueId()); }
+
+    /**
+     * 指定した企画の現在のランキングを取得する
+     * @param event 企画({@link MLSEvent})を指定
+     * @return ランキングリスト({@link Player})を順位, UUID({@link String})の形式で取得
+     */
     public HashMap<Integer, String> getRanking(MLSEvent event) {
         createTable(event);
 
@@ -244,9 +366,9 @@ public class EventSQL {
         return map;
     }
 
-    private void createTable(MLSEvent event) {
-        String sql = "create table if not exists " + event.getDatabase() + " (" +
-                "uuid text NOT NULL PRIMARY KEY," +
+    private void createTable() {
+        String sql = "create table if not exists " + table_name + " (" +
+                "uuid varchar(36) NOT NULL PRIMARY KEY," +
                 "game int(1000) default '0' not null" +
                 "win int(1000) default '0' not null" +
                 ");"
@@ -259,4 +381,18 @@ public class EventSQL {
         }
     }
 
+    private void createTable(MLSEvent event) {
+        String sql = "create table if not exists " + event.getDatabase() + " (" +
+                "uuid varchar(36) NOT NULL PRIMARY KEY," +
+                "game int(1000) default '0' not null" +
+                "win int(1000) default '0' not null" +
+                ");"
+                ;
+        try(Connection con = dataBase.getDataSource().getConnection();
+            PreparedStatement prestat = con.prepareStatement(sql)) {
+            prestat.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
